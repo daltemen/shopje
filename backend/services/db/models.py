3273a3438dab9e23b9c632f71c3284ec99fc2908
Daniel Mendoza <daltemen@gmail.com>
2019-06-22 19:03:38 -0500

    generation db

--- backend/services/db/models.py
+++ backend/services/db/models.py
@@ -1,4 +1,8 @@
-from backend.services.db.db import db
+from flask_sqlalchemy import SQLAlchemy
+from backend.services.app.views import app
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
+db = SQLAlchemy(app)
 
 
 class Shop(db.Model):
